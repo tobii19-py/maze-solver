49e0f990fb64405063e78336104652b4f7a7ffda
tobi <tobi19.py@gmail.com>
2025-02-28 12:22:07 -0600

    Refactored the cell class

---
+++ cell.py
@@ -0,0 +1,31 @@
+from graphics import Line, Point
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            left_wall = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(left_wall, fill_color="red")
+        if self.has_top_wall:
+            top_wall = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(top_wall, fill_color="red")
+        if self.has_right_wall:
+            right_wall = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(right_wall, fill_color="red")
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(bottom_wall, fill_color="red")
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from graphics import Window, Point, Line, Cell
+from graphics import Window, Point, Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    point1 = Point(100, 200)
-    point2 = Point(300, 400)
-    line = Line(point1, point2)
-    cell = Cell(50, 150, 300, 450, win)
-    cell.draw()
-    #win.draw_line(line=line, fill_color="black")
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(125, 125, 200, 200)
+
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(225, 225, 250, 250)
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 main()
