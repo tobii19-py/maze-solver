2c6a27240333171db47bdf6e6e78708fbd1db594
tobii19-py <tobi19.py@gmail.com>
2025-02-27 19:18:35 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.running = False
