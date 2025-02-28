027fb6ef81a0e9cdfa2d4b774758401220ca4f92
tobi <tobi19.py@gmail.com>
2025-02-28 11:11:38 -0600

    Draw left wall

--- graphics.py
+++ graphics.py
@@ -4,10 +4,10 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -39,3 +39,23 @@ class Line:
         canvas.create_line(
             self.__point1.x, self.__point1.y, self.__point2.x, self.__point2.y, fill=fill_color, width=2
         )
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        self.__win = win
+
+    def draw(self, y1, x2):
+        if self.has_left_wall:
+            point1 = Point(self.__x1, self.__y1)
+            point2 = Point(self.__x1, self.__y2)
+
+            left_wall = Line(point1, point2)
+            self.__win.draw_line(left_wall, fill_color="red")
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 def main():
     win = Window(800, 600)
     point1 = Point(100, 200)
     point2 = Point(300, 400)
     line = Line(point1, point2)
+    cell = Cell(50, 150, 300, 450, win)
     win.draw_line(line=line, fill_color="black")
     win.wait_for_close()
 
