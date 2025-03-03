2786c3c95e3555a1e6265363fffeea506d4e7dd1
tobii19-py <tobi19.py@gmail.com>
2025-03-02 18:34:14 -0600

    Break maze entrance and exit walls

--- cell.py
+++ cell.py
@@ -22,15 +22,19 @@ class Cell:
         if self.has_left_wall:
             left_wall = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(left_wall)
+        top_wall = Line(Point(x1, y1), Point(x2, y1))
         if self.has_top_wall:
-            top_wall = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(top_wall)
+        else:
+            self._win.draw_line(top_wall, "white")
         if self.has_right_wall:
             right_wall = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(right_wall)
+        bottom_wall = Line(Point(x1, y2), Point(x2, y2))
         if self.has_bottom_wall:
-            bottom_wall = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(bottom_wall)
+        else:
+            self._win.draw_line(bottom_wall, "white")
     
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self._x2 - self._x1) // 2
