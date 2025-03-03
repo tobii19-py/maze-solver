83a70f9cae463b07b78d82a2d200b850a3671a4f
tobi <tobi19.py@gmail.com>
2025-03-03 15:35:34 -0600

    Reset visited property to False for all cells in maze

--- maze.py
+++ maze.py
@@ -28,6 +28,7 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -103,3 +104,8 @@ class Maze:
                 self._cells[i][j-1].has_bottom_wall = False
 
             self._break_walls_r(next_index[0], next_index[1])
+
+    def _reset_cells_visited(self):
+        for col in self._cells:
+            for cell in col:
+                cell.visited = False
